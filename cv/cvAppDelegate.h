b1a0af47699c0260f26aed2c8fd24d017d6fab39
sonson <yoshida.yuichi@gmail.com>
2011-04-22 18:03:48 +0900

    Make a new project.

---
+++ cv/cvAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  cvAppDelegate.h
+//  cv
+//
+//  Created by sonson on 11/04/22.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class cvViewController;
+
+@interface cvAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet cvViewController *viewController;
+
+@end
---
+++ cv/cvViewController.h
@@ -0,0 +1,15 @@
+//
+//  cvViewController.h
+//  cv
+//
+//  Created by sonson on 11/04/22.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cvViewController : UIViewController {
+    
+}
+
+@end
